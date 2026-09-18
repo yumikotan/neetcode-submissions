f97022bed1f111885ac736f0fd545fc8a3ce9e71
Yumiko Tanido <84880671+yumikotan@users.noreply.github.com>
2026-09-17 23:50:54 -0700

    Add: sql-auto-increment - submission-0

---
+++ SQL/sql-auto-increment/submission-0.sql
@@ -0,0 +1,25 @@
+create sequence gov_id start with 1000 increment by 3;
+
+create table gov_employee (
+  id integer generated always as identity primary key,
+  gov_id integer default nextval('gov_id'),
+  name text
+
+);
+
+
+
+
+
+
+
+
+
+-- Do not modify below this line --
+INSERT INTO gov_employee (name) 
+  VALUES
+      ('John Doe'),
+      ('Jane Doe'),
+      ('Jim Beam');
+
+SELECT * FROM gov_employee;
